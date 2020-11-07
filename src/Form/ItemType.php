0e786aaab930e3052f5d3e347533eece5b76550e
Patxi <patxi064@gmail.com>
2020-11-07 18:24:12 +0100

    Use fix

--- src/Form/ItemType.php
+++ src/Form/ItemType.php
@@ -4,7 +4,6 @@ namespace App\Form;
 
 use App\Entity\Category;
 use App\Entity\Item;
-use App\Entity\Range;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
