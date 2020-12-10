d7ee3b6f08bb498508ef4407ffa802a9e1cdb744
Patxi <patxi064@gmail.com>
2020-12-10 08:32:49 +0100

    Mail => si dev ou test => devmail uniquement

--- src/Controller/AbstractController.php
+++ src/Controller/AbstractController.php
@@ -126,11 +126,11 @@ abstract class AbstractController extends Controller
     protected function fastMail(string $subject, $to, string $template, $viewVars = []): bool
     {
         if (in_array($_ENV['APP_ENV'] ?? null, ['dev', 'test']) && $_ENV['DEV_MAIL'] ?? null) {
-            $to = $_ENV['DEV_MAIL'];
-            $subject = '['.$_ENV['APP_ENV'].']'.$subject;
+            $to = [$_ENV['DEV_MAIL']];
+        } else {
+            $to = is_array($to) ? $to : [$to];
+            $to[] = 'contact@belatika.com';
         }
-        $to = is_array($to) ? $to : [$to];
-        $to[] = 'contact@belatika.com';
         $message = (new Swift_Message($subject))
             ->setCharset('utf-8')
             ->setContentType('text/html')
